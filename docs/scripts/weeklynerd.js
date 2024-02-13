f1f0fae7ef191eb762b8046b58d828da9ad5f070
Eva Boogaard <evaboogaard@hotmail.com>
2024-02-13 20:01:51 +0100

    (feat): weekly nerd

--- docs/scripts/weeklynerd.js
+++ docs/scripts/weeklynerd.js
@@ -1,2 +1,13 @@
 const weeklyNerdButton = document.querySelector('.weeklynerd-button');
 const weeklyNerdContent = document.querySelector('.weeklynerd-content');
+const weeklyNerdCloseButton = document.querySelector(
+    '.weeklynerd-content button'
+);
+
+weeklyNerdButton.addEventListener('click', () => {
+    weeklyNerdContent.classList.add('visible');
+});
+
+weeklyNerdCloseButton.addEventListener('click', () => {
+    weeklyNerdContent.classList.remove('visible');
+});
